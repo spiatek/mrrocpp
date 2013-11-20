784308747f94498ab08451f3729aa31d112bb116
Szymon Piątek <spiatheck@gmail.com>
2013-11-20 18:14:43 +0100

    huge merge

--- src/application/block_move/ecp_t_block_move.cc
+++ src/application/block_move/ecp_t_block_move.cc
@@ -1,38 +1,5 @@
 #include "ecp_t_block_move.h"
 
-<<<<<<< HEAD
-=======
-#include "generator/ecp/smooth_file_from_mp/ecp_g_smooth_file_from_mp.h"
-#include "generator/ecp/bias_edp_force/ecp_g_bias_edp_force.h"
-
-//#include "subtask/ecp_mp_st_gripper_opening.h"
-
-#include "generator/ecp/tff_gripper_approach/ecp_mp_g_tff_gripper_approach.h"
-#include "generator/ecp/newsmooth/ecp_mp_g_newsmooth.h"
-#include "generator/ecp/newsmooth/ecp_g_newsmooth.h"
-#include "generator/ecp/ecp_g_multiple_position.h"
-
-#include "sensor/discode/discode_sensor.h"
-#include "../visual_servoing/visual_servoing.h"
-
-#include "robot/irp6p_m/const_irp6p_m.h"
-#include "robot/irp6p_m/ecp_r_irp6p_m.h"
-//#include "robot/irp6p_tfg/const_irp6p_tfg.h"
-
-#define BOARD_COLOR 5
-
-#define BLOCK_WIDTH 0.0325
-#define BLOCK_HEIGHT 0.0193
-
-#define BLOCK_REACHING 0
-#define BUILDING 1
-
-using namespace mrrocpp::ecp_mp::sensor::discode;
-using namespace mrrocpp::ecp::common::generator;
-using namespace logger;
-using namespace std;
-
->>>>>>> 39ea5d2768963843437c746f344e0d32d87ebd2b
 namespace mrrocpp {
 namespace ecp {
 namespace common {
